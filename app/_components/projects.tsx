e7a554cea34213510eaa9e43f0cdc45016d9d034
devShadow <147122977+kaushal-Prakash@users.noreply.github.com>
2025-03-11 22:49:53 +0530

    added crave and spotify clone

--- app/_components/projects.tsx
+++ app/_components/projects.tsx
@@ -7,22 +7,29 @@ import styles from "@/app/_styles/projects.module.css";
 
 const projectData = [
   {
+    title: "Crave",
+    description:
+      "Crave is a full-stack recipe-sharing platform that enables users to create, share, and explore recipes. Built with Express.js and MySQL, it ensures secure data management and user authentication. The Next.js frontend offers a smooth, responsive experience with a rich text editor for detailed recipe descriptions. The platform also supports media uploads and interactive UI elements, enhancing user engagement.",
+    tags: ["Next", "MySQL", "Express","Tailwind"],
+    image: "/images/crave.png",
+    alt: "Crave App",
+    link: "https://github.com/kaushal-Prakash/crave",
+  },{
+    title: "Spotify Clone",
+    description:
+      "Designed and developed a functional Spotify clone that allows users to upload, manage, and display album images and songs seamlessly. Built using Next.js and React for a dynamic user interface, integrated cloud-based storage solutions for efficient media management, and implemented a secure backend for handling user authentication and data storage.",
+    tags: ["Next", "MongoDb","Tailwind","Cloudinary"],
+    image: "/images/spotify-clone.png",
+    alt: "Spotify Clone App",
+    link: "https://github.com/kaushal-Prakash/spotify-clone",
+  },{
     title: "Attendly",
     description:
       "Imagine an app that flawlessly merges aesthetic appeal with functionality. Attendly achieves this beautifully. Realized through Next.jsand MongoDB, it provides a smooth, user-centric experience.",
-    tags: ["Next", "MongoDb", "Express","Tailwind"],
+    tags: ["Next", "MongoDb","Tailwind"],
     image: "/images/attendly.png",
     alt: "Attendly App",
-    link: "https://nie-attendly.vercel.app/",
-  },
-  {
-    title: "Astoneaone",
-    description:
-      "Astoneaone is the official website for Diamond Group, a leading retail company offering a diverse range of high-quality products across multiple categories. Our platform ensures a seamless shopping experience, providing customers with a wide selection of items to meet their needs and preferences.",
-    tags: ["Next","Nodemailer","Tailwind","Express"],
-    image: "/images/astoneaone.png",
-    alt: "Lakeview Apartments Website",
-    link: "https://astoneaone.vercel.app/",
+    link: "https://github.com/kaushal-Prakash/NIEAttendly",
   },
   {
     title: "Community App",
